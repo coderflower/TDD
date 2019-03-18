6a7e7fad0bd189177ebc60f9ff709cec788d6cbb
coder.flower <coder.flower@gmail.com>
2019-03-18 12:17:19 +0800

    添加测试用例

---
+++ TDDTests/TDDSpec.swift
@@ -0,0 +1,29 @@
+//
+//  TDDSpec.swift
+//  TDDTests
+//
+//  Created by 花菜 on 2019/3/18.
+//  Copyright © 2019 Coder.flower. All rights reserved.
+//
+
+import XCTest
+import Quick
+import Nimble
+@testable import TDD
+class TDDSpec: QuickSpec {
+    override func spec() {
+        var moviesViewController: MoviesViewController!
+        describe("MoviesViewController") {
+            beforeEach {
+                moviesViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as! MoviesViewController
+                // 加载控制器的 view
+                _ = moviesViewController.view
+            }
+            context("when controller's view is loaded", {
+                it("numofRows in tableview section 0 shold be 10", closure: {
+                    expect(moviesViewController.tableView.numberOfRows(inSection: 0)) == 10
+                })
+            })
+        }
+    }
+}
